8affdd571334dffe61a7ab41d73472ec5f5b83ce
Clément Garnier <cle.garnier@gmail.com>
2014-05-15 11:28:17 +0200

    Fix lint error

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,7 +1,7 @@
 (function(){
     'use strict';
 
-    angular.module("app", ['ngRoute'])
+    angular.module('app', ['ngRoute'])
         .config(function($routeProvider){
             $routeProvider
                 .when('/', {
